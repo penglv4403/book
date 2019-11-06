d266bb7f875d446300bb7a61674f8536d7777a13
lvpeng <lvpeng22822@hundsun.com>
2019-11-06 18:27:49 +0800

    阿波罗

--- src/main/java/com/orange/book/BookApplication.java
+++ src/main/java/com/orange/book/BookApplication.java
@@ -1,9 +1,11 @@
 package com.orange.book;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableApolloConfig
 public class BookApplication {
 
     public static void main(String[] args) {
